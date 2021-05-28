f6f903dd4cc1699232082d7822b8e878b88d7a19
aadhuniksolution <dknowledge18@gmail.com>
2021-05-28 15:44:33 +0530

    Phpmailtracking

---
+++ sendmail.php
@@ -0,0 +1,47 @@
+<?php            
+    require("class.phpmailer.php");
+    
+    //
+    $connection = mysqli_connect("localhost","root","password","dbaname");
+    $uniq = uniqid();
+    
+    $mail = new PHPMailer();
+    
+    try
+    {
+          
+            $mail->IsSMTP();
+            $mail->Host = "smtp.gmail.com";
+            $mail->Port = 587;
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = 'tls';
+            
+            $mail->Username = "yourmail";
+            $mail->Password = "yourpassword";
+            
+            $mail->From = "";
+            $mail->FromName = "Aadhuniksolution Digital Marketing Agency";
+            
+            $mail->AddAddress("xyz@gmail.com");
+            
+            $mail->IsHTML(true);
+            
+            $mail->Subject = "Check Mail Status Send , or Open...";
+            $mail->Body = "<img src='https://aadhuniksolution.com/mailstatus.php?token=$uniq' style='height:1px;width:1px;'>Hi,aadhuniksolution i am your subscriber so you can help me ?";
+            
+            if(!$mail->Send())
+            {
+                echo "Sorry! Message Has Been Not Send Some Technical Issue Was Found";
+            }
+            else
+            {
+                echo  "Success! Message Has Been sent to your mail address!";
+                mysqli_query($connection,"insert into cudata (status,uniqid) values ('send','$uniq')");
+            }             
+            
+    }
+    catch (Exception $e)
+    {
+        $msg =  "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+?>
